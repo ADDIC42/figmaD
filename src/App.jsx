68c3f2800225b426419803e6193d42f3172093b3
Адам Саралиев <adamfuturama@mail.ru>
2022-11-24 17:46:49 +0300

    Блин

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Tex from './components/Tex';
+
+const App = () => {
+  return (
+    <>
+   <Tex />
+   <Header />
+   <Main />
+   <Footer />
+   </>
+  );
+};
+
+export default App;
